ac82f93a96653293a899867c2c3c6dba2ba593f8
ben wolfson <wolfson@gmail.com>
2012-04-25 14:33:43 -0700

    while not necessary, this is waaaay faster

--- p97.hs
+++ p97.hs
@@ -1,1 +1,8 @@
-main = putStrLn $ reverse $ take 10 $ reverse $ show $ 28433 * (2 ^ 7830457) + 1
+modexp b e m = modexp' b e m 1
+    where 
+      modexp' b 0 m a = a
+      modexp' b e m a 
+          | e `rem` 2 == 0 = modexp' (b*b) (e `quot` 2) m a
+          | otherwise = modexp' b (e - 1) m ((a * b) `rem` m)
+p = 1 + 28433 * (modexp 2 7830457 100000000000)
+main = print $ p `rem` 10000000000
